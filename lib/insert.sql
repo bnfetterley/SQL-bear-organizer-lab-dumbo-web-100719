98502bd1e7843d3e3e6634439666454e53ebf644
Bri Fetterley <bnfetterley@usfca.edu>
2019-10-17 16:21:17 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate',2, 'male', 'brown', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'male', 'brown', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha',2, 'male', 'brown', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown',2, 'male', 'brown', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa',2, 'male', 'brown', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch',2, 'male', 'brown', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy',2, 'male', 'brown', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null ,2, 'male', 'brown', 'happy', 0);
+
+
+
+    
